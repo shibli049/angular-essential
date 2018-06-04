cdcf2328550a318c7184f86ebb91292bed47bde4
Tahmim Ahmed Shibli <shibli05@gmail.com>
2018-06-05 02:51:25 +0600

    directive and property bindings.

--- src/app/favorite.directive.ts
+++ src/app/favorite.directive.ts
@@ -1,10 +1,18 @@
-import { Directive } from '@angular/core';
+import { Directive, HostBinding, Input } from '@angular/core';
 
 @Directive({
   selector: '[appFavorite]'
 })
 export class FavoriteDirective {
 
+  @HostBinding('class.is-favorite') isFavorite = true;
+
+
   constructor() { }
+
+  @Input() set appFavorite(value) {
+    this.isFavorite = value;
+  }
+
 
 }
