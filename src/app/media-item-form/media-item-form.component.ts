2810d725bf683fe3e77d232fdff47e6f931a724b
Tahmim Ahmed Shibli <shibli05@gmail.com>
2018-06-06 03:09:07 +0600

    built-in and custom validators.

--- src/app/media-item-form/media-item-form.component.ts
+++ src/app/media-item-form/media-item-form.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormGroup, FormControl } from '@angular/forms';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-media-item-form',
@@ -15,14 +15,32 @@ export class MediaItemFormComponent implements OnInit {
   ngOnInit() {
     this.form = new FormGroup({
       medium: new FormControl('Movies'),
-      name: new FormControl(),
-      category: new FormControl(),
-      year: new FormControl()
+      name: new FormControl('', Validators.compose([
+        Validators.pattern('[\\w\\-\\s\\/]+'),
+        Validators.required
+      ])),
+      category: new FormControl(''),
+      year: new FormControl('', this.yearValidator)
     });
   }
 
 
   onSubmit(mediaItem) {
     console.log(mediaItem);
+  }
+
+  // validator return null when valid, else return an object
+  yearValidator(control) {
+    if (control.value.trim().length === 0) {
+      return null;
+    }
+
+    const year = parseInt(control.value, 10);
+    if (year >= 1900 && year <= 2100) {
+      return null;
+    }
+
+    return {'year' : 'invalid'};
+
   }
 }
