5a90e2ff23ac4cc119ad7f2940b47f90e5471250
Tahmim Ahmed Shibli <shibli05@gmail.com>
2018-06-09 02:47:51 +0600

    deleted spec unit test files.

--- src/app/category-list.pipe.spec.ts
+++
@@ -1,8 +0,0 @@
-import { CategoryListPipe } from './category-list.pipe';
-
-describe('CategoryListPipe', () => {
-  it('create an instance', () => {
-    const pipe = new CategoryListPipe();
-    expect(pipe).toBeTruthy();
-  });
-});
--- src/app/favorite.directive.spec.ts
+++
@@ -1,8 +0,0 @@
-import { FavoriteDirective } from './favorite.directive';
-
-describe('FavoriteDirective', () => {
-  it('should create an instance', () => {
-    const directive = new FavoriteDirective();
-    expect(directive).toBeTruthy();
-  });
-});
